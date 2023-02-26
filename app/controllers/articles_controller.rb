e22661f2323ad5fc1cd6c5ab2916cc027dc4eeca
潘心平 <>
2023-02-26 11:22:02 +0900

    authenticate_user!を追記し、ログインユーザーにアクションを絞るよう実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :set_article, only: %i[ edit update destroy ]
+
   def index
     @articles = Article.all
   end
@@ -7,13 +10,42 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(
-      title: params[:title],
-      content: params[:content],
-    )
-    @article.save!
-
+  def new
+    @article = Article.new
   end
 
+  def create
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "#{t('activerecord.models.article')}を作成しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+   if @article.update!(article_params)
+    redirect_to @article, notice: "#{t('activerecord.models.article')}を編集しました。"
+   else
+    render :edit
+   end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_url, notice: "#{t('activerecord.models.article')}を削除しました。"
+  end
+
+ private
+   #ストロングパラメーターの実装
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = current_user.articles.find(params[:id])
+  end
 end
