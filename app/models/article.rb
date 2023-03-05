9b954393a4f2c41a3e4859066db1fbe1dc19a72a
潘心平 <>
2023-03-05 13:23:19 +0900

    ・各テーブルのアソシエーションを実装 ・タグテーブルの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
+
+  has_many :article_tags, dependent: :destroy
+  has_many :tags, through: :article_tags
 end
